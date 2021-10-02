0e01fcb7e86aa4a51135b5a0a0c611b92b7a5ed1
Ben Botvinick <benbotvinick@gmail.com>
2021-10-01 23:55:53 -0400

    Reproduce error

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  reactStrictMode: true,
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [{ type: 'host', value: '(?<subdomain>.*).vercel.(?<extension>group|gg|xyz|wtf|app' }],
+          destination: '/?subdomain=:subdomain&path=:path*&extension=:extension',
+        },
+      ],
+      afterFiles: [],
+      fallback: [],
+    };
+  },
 }
